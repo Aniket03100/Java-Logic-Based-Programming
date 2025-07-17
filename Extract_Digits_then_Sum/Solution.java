ebc134063ce2315af3cc2bdbd97c9d12e89d439f
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-07-17 23:58:55 +0530

    Added Sum of Extracted Digits

---
+++ Extract_Digits_then_Sum/Solution.java
@@ -0,0 +1,31 @@
+// LBP8
+
+// Sum of Digits 
+
+// Implement a program to calculate sum of digits present in the given number
+
+// input -------------> a number from the user
+// constraint --------> n>0
+// output ------------> print sum of digits
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int d, s;
+        s = 0;
+
+        while(n != 0){
+            d = n % 10;
+            s = s + d;
+            n = n / 10;
+        }
+
+        System.out.println(s);
+        obj.close();
+
+    }
+}

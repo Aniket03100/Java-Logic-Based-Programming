6360299ef83e605ab1731a9b5a6809329b13add7
Aniket Bhiungade <aniketbhiungade@gmail.com>
2025-07-20 00:30:26 +0530

    Added Sum of Odd Digits

--- Sum_of_Odd_Digits/Solution.java
+++ Sum_of_Odd_Digits/Solution.java
@@ -1,5 +1,34 @@
+// LBP10
+
+// Sum of odd Digits 
+
+// Implement a program to calculate sum of odd digits present in the given number
+
+// input -------------> a number from the user
+// constraint --------> n>0
+// output ------------> print sum of odd digits
+
+
+
+
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-        
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int s, d;
+        s = 0;
+
+        while(n != 0){
+            d = n % 10;
+            if(n % 2 != 0){
+                s = s + d;
+            }
+            n = n / 10;
+        }
+        System.out.println(s);
+
+        obj.close();
     }
 }
